4238c50ba395e68e728db0d6cd7c96ac928e4fa2
Maxim <lolerpro4@gmail.com>
2025-02-10 23:19:18 +0300

    [fix(auth-service)]: integrate JWT token generation and refresh logic in AuthService

--- services/auth-service/internal/service/auth_service.go
+++ services/auth-service/internal/service/auth_service.go
@@ -4,31 +4,44 @@ import (
 	"errors"
 	"github.com/exPriceD/Streaming-platform/services/auth-service/internal/entities"
 	"github.com/exPriceD/Streaming-platform/services/auth-service/internal/repository"
+	"github.com/exPriceD/Streaming-platform/services/auth-service/internal/token"
+	"time"
 )
 
 type AuthService struct {
-	userRepo repository.UserRepository
+	userRepo   repository.UserRepository
+	tokenRepo  repository.TokenRepository
+	jwtManager *token.JWTManager
 }
 
-func NewAuthService(userRepo repository.UserRepository) *AuthService {
-	return &AuthService{userRepo: userRepo}
+func NewAuthService(userRepo repository.UserRepository, tokenRepo repository.TokenRepository, jwtManager *token.JWTManager) *AuthService {
+	return &AuthService{
+		userRepo:   userRepo,
+		tokenRepo:  tokenRepo,
+		jwtManager: jwtManager,
+	}
 }
 
-func (s *AuthService) Register(username, email, password string, consent bool) (*entities.User, error) {
+func (s *AuthService) Register(username, email, password string, consent bool) (*entities.User, string, string, error) {
 	user, err := entities.NewUser(username, email, password, consent)
 	if err != nil {
-		return nil, err
+		return nil, "", "", err
 	}
 
 	err = s.userRepo.CreateUser(user)
 	if err != nil {
-		return nil, err
+		return nil, "", "", err
 	}
 
-	return user, nil
+	accessToken, refreshToken, err := s.generateTokens(user.ID.String())
+	if err != nil {
+		return nil, "", "", err
+	}
+
+	return user, accessToken, refreshToken, err
 }
 
-func (s *AuthService) Authenticate(identifier, password string, isEmail bool) (*entities.User, error) {
+func (s *AuthService) Authenticate(identifier, password string, isEmail bool) (*entities.User, string, string, error) {
 	var user *entities.User
 	var err error
 
@@ -39,12 +52,55 @@ func (s *AuthService) Authenticate(identifier, password string, isEmail bool) (*
 	}
 
 	if err != nil {
-		return nil, errors.New("the user was not found")
+		return nil, "", "", errors.New("the user was not found")
 	}
 
 	if !user.CheckPassword(password) {
-		return nil, errors.New("invalid password")
+		return nil, "", "", errors.New("invalid password")
 	}
 
-	return user, nil
+	accessToken, refreshToken, err := s.generateTokens(user.ID.String())
+	if err != nil {
+		return nil, "", "", err
+	}
+
+	return user, accessToken, refreshToken, nil
+}
+
+func (s *AuthService) generateTokens(userID string) (string, string, error) {
+	accessToken, err := s.jwtManager.GenerateAccessToken(userID)
+	if err != nil {
+		return "", "", err
+	}
+
+	refreshTokenStr, err := s.jwtManager.GenerateRefreshToken()
+	if err != nil {
+		return "", "", err
+	}
+
+	refreshToken := &entities.RefreshToken{
+		UserID:    userID,
+		Token:     refreshTokenStr,
+		ExpiresAt: time.Now().Add(s.jwtManager.RefreshTokenDuration),
+		Revoked:   false,
+	}
+
+	if err := s.tokenRepo.SaveRefreshToken(refreshToken); err != nil {
+		return "", "", err
+	}
+
+	return accessToken, refreshTokenStr, nil
+}
+
+func (s *AuthService) ValidateAccessToken(tokenStr string) (*token.UserClaims, error) {
+	return s.jwtManager.ValidateToken(tokenStr)
+}
+
+func (s *AuthService) RefreshTokens(refreshTokenStr string) (string, error) {
+	refreshToken, err := s.tokenRepo.GetRefreshToken(refreshTokenStr)
+	if err != nil {
+		return "", err
+	}
+
+	return s.jwtManager.GenerateAccessToken(refreshToken.UserID)
 }
