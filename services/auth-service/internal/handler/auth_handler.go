08f726cb080eed250789987ed0d6a80e2aef75b0
Maxim <lolerpro4@gmail.com>
2025-02-10 22:56:34 +0300

    [feet(auth-service)]: add Login and Register handlers

---
+++ services/auth-service/internal/handler/auth_handler.go
@@ -0,0 +1,73 @@
+package handler
+
+import (
+	"context"
+	"github.com/exPriceD/Streaming-platform/services/auth-service/internal/service"
+	pb "github.com/exPriceD/Streaming-platform/services/auth-service/proto"
+)
+
+type AuthHandler struct {
+	pb.UnimplementedAuthServiceServer
+	authService *service.AuthService
+}
+
+func NewAuthHandler(authService *service.AuthService) *AuthHandler {
+	return &AuthHandler{authService: authService}
+}
+
+func (h *AuthHandler) Register(ctx context.Context, req *pb.RegisterRequest) (*pb.RegisterResponse, error) {
+	user, err := h.authService.Register(req.Username, req.Email, req.Password, req.Consent)
+	if err != nil {
+		return &pb.RegisterResponse{
+			Error: &pb.Error{
+				Code:    pb.ErrorCode_INVALID_ARGUMENT,
+				Message: err.Error(),
+			},
+		}, err
+	}
+
+	return &pb.RegisterResponse{
+		UserId:       user.ID.String(),
+		AccessToken:  "dummy_access_token",
+		RefreshToken: "dummy_refresh_token",
+		ExpiresIn:    3600,
+	}, nil
+}
+
+func (h *AuthHandler) Login(ctx context.Context, req *pb.LoginRequest) (*pb.LoginResponse, error) {
+	var identifier string
+	var isEmail bool
+
+	switch v := req.LoginIdentifier.(type) {
+	case *pb.LoginRequest_Email:
+		identifier = v.Email
+		isEmail = true
+	case *pb.LoginRequest_Username:
+		identifier = v.Username
+		isEmail = false
+	default:
+		return &pb.LoginResponse{
+			Error: &pb.Error{
+				Code:    pb.ErrorCode_INVALID_ARGUMENT,
+				Message: "invalid login identifier",
+			},
+		}, nil
+	}
+
+	user, err := h.authService.Authenticate(identifier, req.Password, isEmail)
+	if err != nil {
+		return &pb.LoginResponse{
+			Error: &pb.Error{
+				Code:    pb.ErrorCode_UNAUTHORIZED,
+				Message: err.Error(),
+			},
+		}, err
+	}
+
+	return &pb.LoginResponse{
+		UserId:       user.ID.String(),
+		AccessToken:  "dummy_access_token",
+		RefreshToken: "dummy_refresh_token",
+		ExpiresIn:    3600,
+	}, nil
+}
