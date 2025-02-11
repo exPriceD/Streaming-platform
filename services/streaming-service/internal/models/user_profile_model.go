e548b1be7bb064514477178a5edea784aeaff044
Viktoria <v.p.persuk@niuitmo.ru>
2025-02-11 20:00:17 +0300

    [feat(streaming-service)]: add Stream and User Profile models

---
+++ services/streaming-service/internal/models/user_profile_model.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type UserProfile struct {
+	ID                 uuid.UUID `db:"id"`                  // Уникальный идентификатор профиля пользователя
+	ChannelDescription string    `db:"channel_description"` // Описание канала пользователя
+	StreamKey          string    `db:"stream_key"`          // Ключ стрима
+	IsLive             bool      `db:"is_live"`             // Флаг, указывающий на то, что пользователь в данный момент стримит
+	CreatedAt          time.Time `db:"created_at"`          // Дата создания записи в БД
+	UpdatedAt          time.Time `db:"updated_at"`          // Дата последнего обновления
+	// ChannelName     string    `db:"channel_name"`        // Название канала пользователя
+}
