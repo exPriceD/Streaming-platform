a1ea227c90fc5a809a88027c8f200aa83b316f1d
Viktoria <v.p.persuk@niuitmo.ru>
2025-02-12 00:33:44 +0300

    fix(streaming-service): add ChannelName column to UserProfile model

--- services/streaming-service/internal/models/user_profile_model.go
+++ services/streaming-service/internal/models/user_profile_model.go
@@ -8,10 +8,10 @@ import (
 
 type UserProfile struct {
 	ID                 uuid.UUID `db:"id"`                  // Уникальный идентификатор профиля пользователя
+	ChannelName        string    `db:"channel_name"`        // Название канала пользователя
 	ChannelDescription string    `db:"channel_description"` // Описание канала пользователя
 	StreamKey          string    `db:"stream_key"`          // Ключ стрима
 	IsLive             bool      `db:"is_live"`             // Флаг, указывающий на то, что пользователь в данный момент стримит
 	CreatedAt          time.Time `db:"created_at"`          // Дата создания записи в БД
 	UpdatedAt          time.Time `db:"updated_at"`          // Дата последнего обновления
-	// ChannelName     string    `db:"channel_name"`        // Название канала пользователя
 }
