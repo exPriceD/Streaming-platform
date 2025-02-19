55f0c9632e40da62a7f32dddc6d8d4ac41c93b27
Maxim <lolerpro4@gmail.com>
2025-02-19 13:33:44 +0300

    [feet]: added user service

---
+++ services/user-service/internal/service/user_service.go
@@ -0,0 +1,85 @@
+package service
+
+import (
+	"context"
+	"errors"
+	authProto "github.com/exPriceD/Streaming-platform/services/auth-service/proto"
+	"github.com/exPriceD/Streaming-platform/services/user-service/internal/clients"
+	"github.com/exPriceD/Streaming-platform/services/user-service/internal/entity"
+	"github.com/exPriceD/Streaming-platform/services/user-service/internal/utils"
+	"log"
+	"log/slog"
+)
+
+type UserRepository interface {
+	CreateUser(user *entity.User) error
+	GetUserByEmail(email string) (*entity.User, error)
+	GetUserByUsername(username string) (*entity.User, error)
+}
+
+type UserService struct {
+	authClient *clients.AuthClient
+	userRepo   UserRepository
+	log        *slog.Logger
+}
+
+func NewUserService(authClient *clients.AuthClient, userRepo UserRepository, logger *slog.Logger) *UserService {
+	return &UserService{authClient: authClient, userRepo: userRepo, log: logger}
+}
+
+func (s *UserService) RegisterUser(username, email, password, confirmPassword string, consent bool) (string, *string, *string, *entity.User, error) {
+	user, err := entity.NewUser(username, email, password, confirmPassword, consent)
+	if err != nil {
+		log.Printf("User creation error: %v", err)
+	}
+
+	err = s.userRepo.CreateUser(user)
+	if err != nil {
+		return "", nil, nil, nil, err
+	}
+
+	resp, err := s.authenticateUser(user.ID.String())
+	if err != nil {
+		return "", nil, nil, nil, err
+	}
+
+	return user.ID.String(), &resp.AccessToken, &resp.RefreshToken, user, nil
+}
+
+func (s *UserService) LoginUser(loginIdentifier, password string) (string, *string, *string, error) {
+	var user *entity.User
+	var err error
+
+	if utils.IsEmail(loginIdentifier) {
+		user, err = s.userRepo.GetUserByEmail(loginIdentifier)
+	} else {
+		user, err = s.userRepo.GetUserByUsername(loginIdentifier)
+	}
+
+	if err != nil {
+		return "", nil, nil, err
+	}
+
+	if !user.CheckPassword(password) {
+		return "", nil, nil, errors.New("incorrect password")
+	}
+
+	resp, err := s.authenticateUser(user.ID.String())
+	if err != nil {
+		return "", nil, nil, err
+	}
+	return user.ID.String(), &resp.AccessToken, &resp.RefreshToken, nil
+}
+
+func (s *UserService) authenticateUser(userID string) (*authProto.AuthenticateResponse, error) {
+	req := &authProto.AuthenticateRequest{UserId: userID}
+	resp, err := s.authClient.Authenticate(context.Background(), req)
+	if err != nil || resp.Error != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+func (s *UserService) GetUserByID(userID string) (*entity.User, error) {
+	return nil, nil
+}
