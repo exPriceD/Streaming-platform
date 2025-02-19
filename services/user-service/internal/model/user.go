8cebf4d51cefe2bc14449b3ef4d25419921cf0ab
Maxim <lolerpro4@gmail.com>
2025-02-19 13:32:12 +0300

    [feet]: added user model

---
+++ services/user-service/internal/model/user.go
@@ -0,0 +1,17 @@
+package model
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type User struct {
+	ID                      uuid.UUID `db:"id"`
+	Username                string    `db:"username"`
+	Email                   string    `db:"email"`
+	PasswordHash            string    `db:"password_hash"`
+	AvatarURL               string    `db:"avatar_url"`
+	ConsentToDataProcessing bool      `db:"consent_to_data_processing"`
+	CreatedAt               time.Time `db:"created_at"`
+	UpdatedAt               time.Time `db:"updated_at"`
+}
