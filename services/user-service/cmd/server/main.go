1bb16d919f6099125300d7dc7900990ecd0a6d11
Maxim <lolerpro4@gmail.com>
2025-02-19 13:36:30 +0300

    [feet]: added an entry point

---
+++ services/user-service/cmd/server/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"database/sql"
+	"github.com/exPriceD/Streaming-platform/config"
+	"github.com/exPriceD/Streaming-platform/pkg/db"
+	"github.com/exPriceD/Streaming-platform/pkg/logger"
+	cl "github.com/exPriceD/Streaming-platform/services/user-service/internal/clients"
+	"github.com/exPriceD/Streaming-platform/services/user-service/internal/repository"
+	"github.com/exPriceD/Streaming-platform/services/user-service/internal/service"
+	router "github.com/exPriceD/Streaming-platform/services/user-service/internal/transport/http"
+	"log/slog"
+)
+
+func main() {
+	log := logger.InitLogger("user-service")
+	clients, err := cl.NewClients("localhost:50051")
+	if err != nil {
+		log.Error("❌ Clients initialization error", slog.String("error", err.Error()))
+	} else {
+		log.Info("✅ Clients are initialized")
+	}
+
+	cfg, err := config.LoadAuthConfig()
+	if err != nil {
+		log.Error("❌ Couldn't load the configuration", slog.String("error", err.Error()))
+	}
+	log.Info("✅ Configuration loaded successfully")
+
+	database, err := db.NewPostgresConnection(cfg.DB)
+	if err != nil {
+		log.Error("❌ Database connection error", slog.String("error", err.Error()))
+		return
+	}
+	defer func(database *sql.DB) {
+		err := database.Close()
+		if err != nil {
+			log.Error("Couldn't close the database", slog.String("error", err.Error()))
+		} else {
+			log.Info("✅ The database connection is closed")
+		}
+	}(database)
+
+	userRepo := repository.NewUserRepository(database)
+	log.Info("🔧 Repositories are initialized")
+
+	userService := service.NewUserService(clients.Auth, userRepo, log)
+	log.Info("🔧 Services are initialized")
+
+	handler := router.NewHandler(userService)
+	log.Info("🔧 Handlers are initialized")
+
+	r := router.NewRouter(handler, log)
+
+	if err := r.Run(":8080"); err != nil {
+		log.Error("❌ Server error", slog.String("error", err.Error()))
+	}
+}
