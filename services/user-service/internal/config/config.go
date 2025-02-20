cb4101503fc515eef270e0b81918e5e7d6e59b35
Maxim <lolerpro4@gmail.com>
2025-02-20 23:43:07 +0300

    [feet]: add config loader

---
+++ services/user-service/internal/config/config.go
@@ -0,0 +1,52 @@
+package config
+
+import (
+	"fmt"
+	"github.com/exPriceD/Streaming-platform/pkg/db"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DBConfig db.DBConfig `mapstructure:"db"`
+	GRPC     GRPC        `mapstructure:"grpc"`
+	HTTP     HTTP        `mapstructure:"http"`
+	Services Services    `mapstructure:"services"`
+}
+
+type GRPC struct {
+	Port int `mapstructure:"port"`
+}
+
+type HTTP struct {
+	Port int `mapstructure:"port"`
+}
+
+type Service struct {
+	Host string `mapstructure:"host"`
+	Port int    `mapstructure:"port"`
+}
+
+type Services struct {
+	AuthService Service `mapstructure:"auth_service"`
+}
+
+func LoadConfig(env string) (*Config, error) {
+	v := viper.New()
+
+	v.AddConfigPath("internal/config")
+	v.SetConfigName(fmt.Sprintf("config.%s", env))
+	v.SetConfigType("yaml")
+
+	v.AutomaticEnv()
+
+	if err := v.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("error reading the config: %v", err)
+	}
+
+	var cfg Config
+	if err := v.Unmarshal(&cfg); err != nil {
+		return nil, fmt.Errorf("config parsing error: %v", err)
+	}
+
+	return &cfg, nil
+}
