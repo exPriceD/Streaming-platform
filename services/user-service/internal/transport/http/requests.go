9b7d84d542ac2ea34776bd58eee2f25cd8f95dd2
Maxim <lolerpro4@gmail.com>
2025-02-21 19:06:01 +0300

    [style]: change struct tags

--- services/user-service/internal/transport/http/requests.go
+++ services/user-service/internal/transport/http/requests.go
@@ -4,11 +4,11 @@ type RegisterRequest struct {
 	Username        string `json:"username"`
 	Email           string `json:"email"`
 	Password        string `json:"password"`
-	ConfirmPassword string `json:"confirmPassword"`
+	ConfirmPassword string `json:"confirm_password"`
 	Consent         bool   `json:"consent"`
 }
 
 type LoginRequest struct {
-	LoginIdentifier string `json:"loginIdentifier"`
+	LoginIdentifier string `json:"login_identifier"`
 	Password        string `json:"password"`
 }
