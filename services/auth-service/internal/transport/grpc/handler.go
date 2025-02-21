f99f6eb2666ca8b92205397d334a1cb886046f5a
Maxim <lolerpro4@gmail.com>
2025-02-22 01:42:56 +0300

    [style]: changed variables name

--- services/auth-service/internal/transport/grpc/handler.go
+++ services/auth-service/internal/transport/grpc/handler.go
@@ -1,9 +1,9 @@
-package handler
+package grpcTransport
 
 import (
 	"context"
 	"errors"
-	"github.com/exPriceD/Streaming-platform/pkg/proto/v1/auth"
+	pb "github.com/exPriceD/Streaming-platform/pkg/proto/v1/auth"
 	"github.com/exPriceD/Streaming-platform/services/auth-service/internal/service"
 	"github.com/google/uuid"
 	"google.golang.org/protobuf/types/known/timestamppb"
@@ -18,28 +18,28 @@ type AuthService interface {
 	Logout(ctx context.Context, refreshToken string) error
 }
 
-type AuthHandler struct {
-	auth.UnimplementedAuthServiceServer
+type Handler struct {
+	pb.UnimplementedAuthServiceServer
 	authService AuthService
-	log         *slog.Logger
+	logger      *slog.Logger
 }
 
-func NewAuthHandler(authService AuthService, log *slog.Logger) *AuthHandler {
-	return &AuthHandler{
+func NewAuthHandler(authService AuthService, logger *slog.Logger) *Handler {
+	return &Handler{
 		authService: authService,
-		log:         log,
+		logger:      logger,
 	}
 }
 
-func (h *AuthHandler) Authenticate(ctx context.Context, req *auth.AuthenticateRequest) (*auth.AuthenticateResponse, error) {
-	h.log.Info("Received Authenticate request", slog.String("user_id", req.UserId))
+func (h *Handler) Authenticate(ctx context.Context, req *pb.AuthenticateRequest) (*pb.AuthenticateResponse, error) {
+	h.logger.Info("Received Authenticate request", slog.String("user_id", req.UserId))
 
 	userID, err := uuid.Parse(req.UserId)
 	if err != nil {
-		h.log.Warn("Invalid User ID in Authenticate request", slog.String("user_id", req.UserId))
-		return &auth.AuthenticateResponse{
-			Error: &auth.Error{
-				Code:    auth.ErrorCode_INVALID_ARGUMENT,
+		h.logger.Warn("Invalid User ID in Authenticate request", slog.String("user_id", req.UserId))
+		return &pb.AuthenticateResponse{
+			Error: &pb.Error{
+				Code:    pb.ErrorCode_INVALID_ARGUMENT,
 				Message: "Invalid User ID",
 			},
 		}, err
@@ -47,17 +47,17 @@ func (h *AuthHandler) Authenticate(ctx context.Context, req *auth.AuthenticateRe
 
 	accessToken, refreshToken, expiresIn, expiresAt, err := h.authService.Authenticate(ctx, userID)
 	if err != nil {
-		h.log.Error("Failed to authenticate user", slog.String("user_id", userID.String()), slog.String("error", err.Error()))
-		return &auth.AuthenticateResponse{
-			Error: &auth.Error{
-				Code:    auth.ErrorCode_INTERNAL_ERROR,
+		h.logger.Error("Failed to authenticate user", slog.String("user_id", userID.String()), slog.String("error", err.Error()))
+		return &pb.AuthenticateResponse{
+			Error: &pb.Error{
+				Code:    pb.ErrorCode_INTERNAL_ERROR,
 				Message: err.Error(),
 			},
 		}, err
 	}
 
-	h.log.Info("User authenticated", slog.String("user_id", userID.String()))
-	return &auth.AuthenticateResponse{
+	h.logger.Info("User authenticated", slog.String("user_id", userID.String()))
+	return &pb.AuthenticateResponse{
 		AccessToken:  accessToken,
 		RefreshToken: refreshToken,
 		ExpiresIn:    expiresIn,
@@ -65,38 +65,38 @@ func (h *AuthHandler) Authenticate(ctx context.Context, req *auth.AuthenticateRe
 	}, nil
 }
 
-func (h *AuthHandler) ValidateToken(ctx context.Context, req *auth.ValidateTokenRequest) (*auth.ValidateTokenResponse, error) {
-	h.log.Info("Received ValidateToken request")
+func (h *Handler) ValidateToken(ctx context.Context, req *pb.ValidateTokenRequest) (*pb.ValidateTokenResponse, error) {
+	h.logger.Info("Received ValidateToken request")
 
 	userID, err := h.authService.ValidateToken(ctx, req.AccessToken)
 	if err != nil {
-		h.log.Warn("Invalid access token", slog.String("error", err.Error()))
-		return &auth.ValidateTokenResponse{
+		h.logger.Warn("Invalid access token", slog.String("error", err.Error()))
+		return &pb.ValidateTokenResponse{
 			Valid: false,
 			Error: mapErrorToProto(err),
 		}, nil
 	}
 
-	h.log.Info("Access token validated", slog.String("user_id", userID.String()))
-	return &auth.ValidateTokenResponse{
+	h.logger.Info("Access token validated", slog.String("user_id", userID.String()))
+	return &pb.ValidateTokenResponse{
 		Valid:  true,
 		UserId: userID.String(),
 	}, nil
 }
 
-func (h *AuthHandler) RefreshToken(ctx context.Context, req *auth.RefreshTokenRequest) (*auth.RefreshTokenResponse, error) {
-	h.log.Info("Received RefreshToken request")
+func (h *Handler) RefreshToken(ctx context.Context, req *pb.RefreshTokenRequest) (*pb.RefreshTokenResponse, error) {
+	h.logger.Info("Received RefreshToken request")
 
 	newAccessToken, newRefreshToken, expiresIn, expiresAt, err := h.authService.RefreshTokens(ctx, req.RefreshToken)
 	if err != nil {
-		h.log.Warn("Failed to refresh tokens", slog.String("error", err.Error()))
-		return &auth.RefreshTokenResponse{
+		h.logger.Warn("Failed to refresh tokens", slog.String("error", err.Error()))
+		return &pb.RefreshTokenResponse{
 			Error: mapErrorToProto(err),
 		}, err
 	}
 
-	h.log.Info("Tokens refreshed successfully")
-	return &auth.RefreshTokenResponse{
+	h.logger.Info("Tokens refreshed successfully")
+	return &pb.RefreshTokenResponse{
 		AccessToken:  newAccessToken,
 		RefreshToken: newRefreshToken,
 		ExpiresIn:    expiresIn,
@@ -104,45 +104,45 @@ func (h *AuthHandler) RefreshToken(ctx context.Context, req *auth.RefreshTokenRe
 	}, nil
 }
 
-func (h *AuthHandler) Logout(ctx context.Context, req *auth.LogoutRequest) (*auth.LogoutResponse, error) {
-	h.log.Info("Received Logout request")
+func (h *Handler) Logout(ctx context.Context, req *pb.LogoutRequest) (*pb.LogoutResponse, error) {
+	h.logger.Info("Received Logout request")
 
 	err := h.authService.Logout(ctx, req.RefreshToken)
 	if err != nil {
-		h.log.Warn("Failed to logout user", slog.String("error", err.Error()))
-		return &auth.LogoutResponse{
+		h.logger.Warn("Failed to logout user", slog.String("error", err.Error()))
+		return &pb.LogoutResponse{
 			Success: false,
-			Error: &auth.Error{
-				Code:    auth.ErrorCode_INTERNAL_ERROR,
+			Error: &pb.Error{
+				Code:    pb.ErrorCode_INTERNAL_ERROR,
 				Message: err.Error(),
 			},
 		}, err
 	}
 
-	h.log.Info("User logged out successfully")
-	return &auth.LogoutResponse{Success: true}, nil
+	h.logger.Info("User logged out successfully")
+	return &pb.LogoutResponse{Success: true}, nil
 }
 
-func mapErrorToProto(err error) *auth.Error {
+func mapErrorToProto(err error) *pb.Error {
 	switch {
 	case errors.Is(err, service.ErrTokenExpired):
-		return &auth.Error{
-			Code:    auth.ErrorCode_TOKEN_EXPIRED,
+		return &pb.Error{
+			Code:    pb.ErrorCode_TOKEN_EXPIRED,
 			Message: "The token has expired",
 		}
 	case errors.Is(err, service.ErrTokenInvalid):
-		return &auth.Error{
-			Code:    auth.ErrorCode_TOKEN_INVALID,
+		return &pb.Error{
+			Code:    pb.ErrorCode_TOKEN_INVALID,
 			Message: "Invalid token",
 		}
 	case errors.Is(err, service.ErrRefreshTokenRevoked):
-		return &auth.Error{
-			Code:    auth.ErrorCode_REFRESH_TOKEN_REVOKED,
+		return &pb.Error{
+			Code:    pb.ErrorCode_REFRESH_TOKEN_REVOKED,
 			Message: "Refresh token has been revoked",
 		}
 	default:
-		return &auth.Error{
-			Code:    auth.ErrorCode_INTERNAL_ERROR,
+		return &pb.Error{
+			Code:    pb.ErrorCode_INTERNAL_ERROR,
 			Message: "Internal server error",
 		}
 	}
